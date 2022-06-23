f8e5f6c8c29399be6524b1dc252cf1b0235dd7ea
Keijsan <kakuzukyo2000@gmail.com>
2022-06-23 11:55:35 +0700

    upload code

---
+++ ecommerce/store/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django import forms
+import re
+
+class CustomUserForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2', 'placeholder': 'Enter username'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2', 'placeholder': 'Enter email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control my-2', 'placeholder': 'Enter password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control my-2', 'placeholder': 'Confirm password'}))
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        # nam@gmail.com
+
+        if not (re.fullmatch(regex, self.cleaned_data.get("email"))):
+            raise forms.ValidationError('Invalid email')
+
+        return self.cleaned_data.get("email")
+
---
+++ ecommerce/store/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+from store.controller import authview, cart, checkout, order
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('categories', views.categories, name='categories'),
+    path('categories/<str:slug>', views.categoriesview, name='categoriesview' ),
+    path('categories/<str:cate_slug>/<str:prod_slug>', views.productview, name='productview'),
+    
+    path('register/', authview.register, name='register'),
+    path('login/', authview.loginpage, name='loginpage'),   
+    path('logout/', authview.logoutpage, name='logout'),
+
+    path('add-to-cart', cart.addtocart, name='addtocart'),
+    path('cart', cart.viewcart, name='cart'),
+    path('update-cart', cart.updatecart, name='updatecart'),
+    path('delete-cart-item', cart.deletecartitem, name='deletecartitem'),
+
+    path('checkout', checkout.index, name='checkout'),
+    path('place-order', checkout.placeorder, name='placeorder'),
+
+    path('my-orders', order.index, name='myorders'),
+    path('view-order/<str:t_no>', order.vieworder, name='orderview'),
+]
